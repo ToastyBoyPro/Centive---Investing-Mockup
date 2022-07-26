757af412237a92fe24c81ea369ac74386221c4d1
Robert Park <toastyboyproductions@gmail.com>
2022-07-26 10:55:39 -0500

    "first commit"

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import { Button } from '../ButtonElements'
+
+import Video from '../../videos/video.mp4'
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight
+} from './HeroElements'
+
+function HeroSection() {
+  const [hover, setHover] = useState(false)
+
+  const onHover = () => {
+    setHover(!hover);
+  }
+  return (
+    <HeroContainer id='home'>
+      <HeroBg>
+        <VideoBg playsInline autoPlay loop muted src={Video} type='video/mp4' />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Investing</HeroH1>
+        <HeroH1>The Simple and Secure Way</HeroH1>
+        <HeroP>
+          Sign up today and receive $200 in credit towards
+          your new account.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to='signup'
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact='true'
+            offset={-80}
+            primary='true'
+            dark='true'
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+          >
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  )
+}
+
+export default HeroSection
